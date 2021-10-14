61d8df457a9cd630cb786610cae74393a6b13d2f
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-14 14:41:25 -0600

    Create get single snack method

--- app/services/snack_service.rb
+++ app/services/snack_service.rb
@@ -5,6 +5,12 @@ class SnackService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def self.get_single_snack(snack_id)
+    response = conn.get("/api/v1/snacks/#{snack_id}")
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def self.conn
     Faraday.new(url: 'https://lit-reaches-91268.herokuapp.com/')
   end
