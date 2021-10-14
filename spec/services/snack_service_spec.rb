7a50482902213b98c1f99369039310df4dd0c8f4
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-14 14:40:03 -0600

    Create test for get single snack method

--- spec/services/snack_service_spec.rb
+++ spec/services/snack_service_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe SnackService do
       expect(snacks[:data][:attributes][:snacks][0][:name]).to eq('Funyuns')
     end
   end
+
+  describe '#get_single_snack' do
+    it 'returns a snack for given id' do
+      stub_body = File.open('spec/fixtures/single_snack.json')
+
+      stub_request(:get, "https://lit-reaches-91268.herokuapp.com/api/v1/snacks/1")
+        .to_return(status: 200, body: stub_body, headers: {})
+
+      snack = SnackService.get_single_snack(1)
+
+      expect(snack[:data][:attributes][:name]).to eq('Funyuns')
+    end
+  end
 end
