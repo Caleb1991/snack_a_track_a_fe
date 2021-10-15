c3dd4dc9d5356059bfb8a53e0a1b2b9ec33c2509
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-15 09:27:22 -0600

    Add login link to welcome page

--- spec/controllers/welcome/index_spec.rb
+++ spec/controllers/welcome/index_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Welcome Page' do
   it 'has a login button' do
-    visit root_path
+    visit '/front_end/v1/welcome'
 
-    expect(page).to have_button('Login')
+    expect(page).to have_link('Login')
   end
 end
