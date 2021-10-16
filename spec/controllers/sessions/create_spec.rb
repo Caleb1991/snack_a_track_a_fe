592b1d041f02b74aa3096c8aac54a419507ed1c6
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-16 17:05:50 -0600

    Create functionality to see users username on their show page

--- spec/controllers/sessions/create_spec.rb
+++ spec/controllers/sessions/create_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Sessions Controller' do
   describe '#create' do
     it 'logs a user in' do
-      stub_body = File.open('spec/fixtures/user_login.json')
+      login_stub_body = File.open('spec/fixtures/user_login.json')
+      user_page_stub_body = File.open('spec/fixtures/gao113211.json')
 
       visit '/welcome'
 
@@ -11,7 +12,10 @@ RSpec.describe 'Sessions Controller' do
       fill_in :password, with: 'Password1'
 
       stub_request(:post, "https://lit-reaches-91268.herokuapp.com/api/v1/sessions")
-        .to_return(status: 200, body: stub_body, headers: {})
+        .to_return(status: 200, body: login_stub_body, headers: {})
+
+      stub_request(:get, "https://lit-reaches-91268.herokuapp.com/api/v1/users/4")
+        .to_return(status: 200, body: user_page_stub_body, headers: {})
 
       click_on 'Login'
 
--- spec/controllers/users/show_spec.rb
+++ spec/controllers/users/show_spec.rb
@@ -2,20 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'User Show Page' do
   before :each do
-    stub_body = File.open('spec/fixtures/user_login.json')
+    login_stub_body = File.open('spec/fixtures/user_login.json')
+    user_page_stub_body = File.open('spec/fixtures/gao113211.json')
 
     visit '/welcome'
 
     fill_in :username, with: 'Gao113211'
     fill_in :password, with: 'Password1'
 
+    stub_request(:get, "https://lit-reaches-91268.herokuapp.com/api/v1/users/4")
+      .to_return(status: 200, body: user_page_stub_body, headers: {})
+
     stub_request(:post, "https://lit-reaches-91268.herokuapp.com/api/v1/sessions")
-      .to_return(status: 200, body: stub_body, headers: {})
+      .to_return(status: 200, body: login_stub_body, headers: {})
 
     click_on 'Login'
   end
 
   it 'has a users username' do
-    expect(page).to have_content('Welcome, Gao11321!')
+    expect(current_path).to eq('/users/4')
+    expect(page).to have_content('Welcome, Gao113211!')
   end
 end
