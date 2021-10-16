40034ae7fd91ffa7369305cf8f02365753d79e85
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-16 14:09:13 -0600

    Add tests for registering a user

---
+++ spec/controllers/welcome/register_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Welcome register page' do
+  it 'has a form to register' do
+    visit '/welcome'
+
+    click_on 'Register'
+
+    expect(current_path).to eq('/welcome/register')
+
+    expect(page).to have_field(:username)
+    expect(page).to have_field(:first_name)
+    expect(page).to have_field(:last_name)
+    expect(page).to have_field(:email)
+    expect(page).to have_field(:password)
+    expect(page).to have_field(:password_confirmation)
+  end
+end
