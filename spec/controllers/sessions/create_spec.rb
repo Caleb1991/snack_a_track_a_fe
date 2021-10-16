352b8898ac766c0642cd9383e83ba01947b89a9e
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-16 16:50:25 -0600

    Update test to green

--- spec/controllers/sessions/create_spec.rb
+++ spec/controllers/sessions/create_spec.rb
@@ -2,20 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Sessions Controller' do
   describe '#create' do
-    xit 'logs a user in' do
+    it 'logs a user in' do
+      stub_body = File.open('spec/fixtures/user_login.json')
+
       visit '/welcome'
 
       fill_in :username, with: 'Gao113211'
       fill_in :password, with: 'Password1'
+
+      stub_request(:post, "https://lit-reaches-91268.herokuapp.com/api/v1/sessions")
+        .to_return(status: 200, body: stub_body, headers: {})
+
       click_on 'Login'
-
-      stub_body = File.open('spec/fixtures/user_login.json')
-
-      stub_request(:post, "https://lit-reaches-91268.herokuapp.com/api/v1/sessions")
-        .to_return(status: 200, body: stub_body, headers: {})
-
-      stub_request(:post, "https://lit-reaches-91268.herokuapp.com/api/v1/sessions")
-        .to_return(status: 200, body: stub_body, headers: {})
 
       expect(current_path).to eq("/users/4")
     end
