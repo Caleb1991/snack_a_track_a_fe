d816a40fed55f9f3376bae5ced4f2fc37c6d6e93
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-17 13:44:26 -0600

    Add a root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   resources :welcome, only: :index
   resources :sessions, only: [:create, :destroy]
   resources :users, only: [:show, :new, :create]
