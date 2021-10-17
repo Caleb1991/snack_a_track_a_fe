90f48dd76d2882c770c169e96ebdad00f4d095c9
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-17 14:03:02 -0600

    Create Snack show page

--- spec/controllers/snacks/index_spec.rb
+++ spec/controllers/snacks/index_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'Snack Index' do
   before :each do
+    stub_body = File.open('spec/fixtures/all_snacks.json')
+
+    stub_request(:get, "https://lit-reaches-91268.herokuapp.com/api/v1/snacks")
+      .to_return(status: 200, body: stub_body, headers: {})
+
     visit '/snacks'
   end
 
